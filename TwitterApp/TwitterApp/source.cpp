252ddb9bf1d4d756894dcaf2e373a20af8e15dda
Max Korner <kornerm@icloud.com>
2019-01-31 17:11:57 +0000

    New Master original

---
+++ TwitterApp/TwitterApp/source.cpp
@@ -0,0 +1,34 @@
+//
+//  source.cpp
+//  TwitterApp
+//
+//  Created by Max Korner on 31/01/2019.
+//  Copyright © 2019 Max Korner. All rights reserved.
+//
+
+#include "source.hpp"
+#include <iostream>
+#include <string>
+#include <fstream>
+
+using namespace std;
+
+void start(){
+    
+    string name;
+    cout << "Please Enter Your First Name";
+    cin >> name;
+    
+    string surName;
+    cout << "Please Enter Your Last Name";
+    cin >> surName;
+    
+    string fullName = name + "
+    
+}
+
+int main() {
+    
+    
+    return 0;
+}
---
+++ TwitterApp/TwitterApp/source.hpp
@@ -0,0 +1,14 @@
+//
+//  source.hpp
+//  TwitterApp
+//
+//  Created by Max Korner on 31/01/2019.
+//  Copyright © 2019 Max Korner. All rights reserved.
+//
+
+#ifndef source_hpp
+#define source_hpp
+
+#include <stdio.h>
+
+#endif /* source_hpp */
